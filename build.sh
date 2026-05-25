2dd48fc1d7b383f67411c349e0f6fc72ec15131a
Chamba718 <miguelzapata1716@gmail.com>
2026-05-25 07:16:50 -0500

    Auto-create superuser script

--- build.sh
+++ build.sh
@@ -6,3 +6,15 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Crear superusuario automáticamente si se configuran las variables
+python manage.py shell -c "
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+email = os.environ.get('SUPERUSER_EMAIL')
+password = os.environ.get('SUPERUSER_PASSWORD')
+if email and password and not User.objects.filter(email=email).exists():
+    user = User.objects.create_superuser(email=email, username=email, password=password, full_name='Administrador', role='admin')
+    print('Superusuario creado exitosamente.')
+"
